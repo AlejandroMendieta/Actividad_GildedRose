a754871ae95f23eec6b52c12a683393fa2ffffad
AlejandroMendieta <49075742+AlejandroMendieta@users.noreply.github.com>
2019-03-29 15:17:20 -0400

    GildedRose - Jose Alejandro Mendieta

---
+++ Actividad_Opcional_Modulo4/Actividad_Opcional_Modulo4/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Actividad_Opcional_Modulo4
+{
+    public class Program
+    {
+        private readonly IUpdateStrategyFactory _strategyFactory = new UpdateStrategyFactory();
+        List<Item> Items;
+
+        public List<Item> GetItems
+        {
+            get { return Items; }
+            set { Items = value; }
+        }
+
+        public Program(List<Item> items)
+        {
+            this.Items = items;
+        }
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Actividad GildedRose");
+
+
+
+            List<Item> Items = new List<Item>
+                {
+                    new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
+                    new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
+                    new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
+                    new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80},
+                    new Item {Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 15, Quality = 20},
+                    new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6}
+                };
+
+            var app = new Program(Items);
+            app.UpdateQuality();
+            System.Console.WriteLine(Items[0].Name + "-" + Items[0].SellIn + "-" + Items[0].Quality);
+            System.Console.WriteLine(Items[1].Name + "-" + Items[1].SellIn + "-" + Items[1].Quality);
+            System.Console.WriteLine(Items[2].Name + "-" + Items[2].SellIn + "-" + Items[2].Quality);
+            System.Console.WriteLine(Items[3].Name + "-" + Items[3].SellIn + "-" + Items[3].Quality);
+            System.Console.WriteLine(Items[4].Name + "-" + Items[4].SellIn + "-" + Items[4].Quality);
+            System.Console.WriteLine(Items[5].Name + "-" + Items[5].SellIn + "-" + Items[5].Quality);
+            System.Console.ReadKey();
+
+        }
+
+        public void UpdateQuality()
+        {
+            foreach (Item item in Items)
+            {
+                var updateStrategy = _strategyFactory.Create(item.Name);
+                updateStrategy.UpdateQuality(item);
+            }
+        }
+    }
+}
---
+++ Actividad_Opcional_Modulo4/Actividad_Opcional_Modulo4_Test/UnitTest1.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using Actividad_Opcional_Modulo4;
+
+namespace Actividad_Opcional_Modulo4_Test
+{
+    [TestClass]
+    public class GlidedRoseTest
+    {
+        [TestMethod]
+        public void testDecrementQuality()
+        {
+            List<Item> Items = new List<Item>()
+            {
+                new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20}
+            };
+
+            var app = new Program(Items);
+            app.UpdateQuality();
+            Assert.AreEqual(19, Items[0].Quality);
+        }
+
+        [TestMethod]
+        public void testDecrementX2Quality() 
+        {
+            List<Item> Items = new List<Item>()
+            {
+                new Item {Name = "+5 Dexterity Vest", SellIn = 0, Quality = 20}
+            };
+
+            var app = new Program(Items);
+            app.UpdateQuality();
+            Assert.AreEqual(18, Items[0].Quality);
+        }
+
+        [TestMethod]
+        public void testDecrementNonNegativeQuality() 
+        {
+            List<Item> Items = new List<Item>()
+            {
+                new Item {Name = "+5 Dexterity Vest", SellIn = 20, Quality = 0}
+            };
+
+            var app = new Program(Items);
+            app.UpdateQuality();
+            Assert.AreEqual(0, Items[0].Quality);
+        }
+
+        [TestMethod]
+        public void testIncrementQuality()
+        {
+            List<Item> Items = new List<Item>()
+            {
+                new Item {Name = "Aged Brie", SellIn = 2, Quality = 0}
+            };
+            var app = new Program(Items);
+            app.UpdateQuality();
+            Assert.AreEqual(1, Items[0].Quality);
+        }
+
+        [TestMethod]
+        public void testIncrementQualityNoMoreThan50() 
+        {
+            List<Item> Items = new List<Item>()
+            {
+                new Item {Name = "Aged Brie", SellIn = 2, Quality = 50}
+            };
+
+            var app = new Program(Items);
+            app.UpdateQuality();
+            Assert.AreEqual(50, Items[0].Quality);
+        }
+
+        [TestMethod]
+        public void testIncrementLegendary() 
+        {
+            List<Item> Items = new List<Item>()
+            {
+                new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80}
+            };
+            var app = new Program(Items);
+            app.UpdateQuality();
+            Assert.AreEqual(80, Items[0].Quality);
+        }
+
+        [TestMethod]
+        public void testIncrementBackstage() 
+        {
+            List<Item> Items = new List<Item>()
+            {
+                new Item {Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 15, Quality = 20}
+            };
+            var app = new Program(Items);
+            app.UpdateQuality();
+            Assert.AreEqual(21, Items[0].Quality);
+        }
+
+        [TestMethod]
+        public void testIncrementBackstagex2()
+        {
+            List<Item> Items = new List<Item>()
+            {
+                new Item {Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 5, Quality = 20}
+            };
+            var app = new Program(Items);
+            app.UpdateQuality();
+            Assert.AreEqual(23, Items[0].Quality);
+        }
+
+        [TestMethod]
+        public void testIncrementBackstagex3()
+        {
+            List<Item> Items = new List<Item>()
+            {
+                new Item {Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 10, Quality = 20}
+            };
+            var app = new Program(Items);
+            app.UpdateQuality();
+            Assert.AreEqual(22, Items[0].Quality);
+        }
+
+        [TestMethod]
+        public void testIncrementBackstagex4()
+        {
+            List<Item> Items = new List<Item>()
+            {
+                new Item {Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 4, Quality = 20}
+            };
+            var app = new Program(Items);
+            app.UpdateQuality();
+            Assert.AreEqual(23, Items[0].Quality);
+        }
+
+        [TestMethod]
+        public void testIncrementBackstagex5()
+        {
+            List<Item> Items = new List<Item>()
+            {
+                new Item {Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 0, Quality = 20}
+            };
+            var app = new Program(Items);
+            app.UpdateQuality();
+            Assert.AreEqual(0, Items[0].Quality);
+        }
+
+        [TestMethod]
+        public void testDecrementConjured()
+        {
+            List<Item> Items = new List<Item>()
+            {
+                new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6}
+            };
+            var app = new Program(Items);
+            app.UpdateQuality();
+            Assert.AreEqual(4, Items[0].Quality);
+        }
+
+        [TestMethod]
+        public void testDecrementConjuredx2()
+        {
+            List<Item> Items = new List<Item>()
+            {
+                new Item {Name = "Conjured Mana Cake", SellIn = 0, Quality = 8}
+            };
+            var app = new Program(Items);
+            app.UpdateQuality();
+            Assert.AreEqual(4, Items[0].Quality);
+        }
+
+        [TestMethod]
+        public void testDecrementConjuredNonNegative()
+        {
+            List<Item> Items = new List<Item>()
+            {
+                new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 0}
+            };
+            var app = new Program(Items);
+            app.UpdateQuality();
+            Assert.AreEqual(0, Items[0].Quality);
+        }
+    }
+}
